771faf248cce9416ca866c450ce9fe391d635d82
Willis Aybar <willisaybar@gmail.com>
2020-04-16 00:52:14 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -5,10 +5,10 @@ class Costume < ActiveRecord::Migration[5.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.string :price
+      t.integer :price
       t.integer :size
       t.string :image_url
-      t.timestamps 
+      t.timestamps
     end
   end
 end
