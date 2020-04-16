9af591f6d538417f6c737f33cd3e75bfe5a4ab69
Willis Aybar <willisaybar@gmail.com>
2020-04-16 01:10:24 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,28 @@
 # Create your haunted_houses migration here
+class HauntedHouse < ActiveRecord::Migration[5.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :description
+    end
+  end
+end
+
+
+
+The haunted_houses table will have eight columns:
+
+name
+location
+theme
+price
+whether they're family friendly or not
+opening date
+closing date
+long description
