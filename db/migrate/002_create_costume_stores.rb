5249e5d81b465f1ae1c710ae7cd254e4182df1b5
Willis Aybar <willisaybar@gmail.com>
2020-04-16 00:59:49 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
 # Create your costume_stores migration here
+class CostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.integer :size
+      t.string :image_url
+      t.timestamps
+    end
+  end
+end
