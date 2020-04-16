a64e69c3bd02a09fc16f38eb129307ff95dcc961
Willis Aybar <willisaybar@gmail.com>
2020-04-16 01:04:47 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,9 @@ class CostumeStores < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :employee_count
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t. :opening_time
 
       t.timestamps
     end
